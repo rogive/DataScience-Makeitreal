49c49e7878dd7460dde57dd689d115b6f810d1bf
ivan <ierodriguezgo@gmail.com>
2020-10-09 22:20:06 -0500

    upgrade

--- algoritmos/leetcode/fibonaccinumber.py
+++
@@ -1,7 +0,0 @@
-def fib(N: int) -> int:
-  if N <= 1:
-      return N
-  else:
-      return fib(N-1) + fib(N-2)
-
-print(fib(4))
--- practica0/practica1.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input('Escoge un número -> '))
-if num1 > 10 :
-  print('Tu número es una decena')
-elif num1 < 10 and num1 > 4 :
-  print ('Tu numero esta entre 4 y 10')
-else:
-  print(f'El número es: {num1}')
